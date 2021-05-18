95c65e1853dd67445cb7727303a2d9e23887ec6b
karolwasemann <kw.kwaq@gmail.com>
2021-05-18 08:58:38 +0200

    Detection without logic

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 import './style.css';
 
 document.querySelector('.display').textContent = '1234';
+
+const display = document.querySelector('.display');
+const keyButtons = document.querySelectorAll('.keyboard__button');
+
+keyButtons.forEach(keyButton => {
+  keyButton.addEventListener('click', () => {
+    const keyNumber = keyButton.getAttribute('data-value');
+    if (keyNumber == -99) {
+      display.innerHTML = '';
+    }
+  });
+});
+
+const reset = () => {
+  display.innerHTML = '';
+};
